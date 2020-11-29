8ea602b1090504e032b21ae356504b5b2ba0ba33
achang <54963910+cying4@users.noreply.github.com>
2020-11-30 02:35:35 +0800

    Update changhao ying_mlp.py

--- Code/changhao ying_mlp.py
+++ Code/changhao ying_mlp.py
@@ -2,33 +2,46 @@ import os
 import numpy as np
 from keras.utils import to_categorical
 #%%
-DATA_DIR = "/home/ubuntu/mlproj/target/train/Fight/"
-x_train_fight =[]
-y_train_fight=[]
+DATA_DIR = "/home/ubuntu/mlproj/video/train/Fight/"
+x_fight =[]
+y_fight=[]
 for path in [f for f in os.listdir(DATA_DIR) if f[-4:] == ".npy"]:
     a = np.load(DATA_DIR + path)
     a=a.reshape(len(a),-1)
     b=np.ones(len(a))
-    y_train_fight.append(b)
-    x_train_fight.append(a)
-x_train_fight=np.vstack(x_train_fight)
-y_train_fight=np.hstack(y_train_fight)
-y_train_fight=np.asarray(y_train_fight,dtype='uint8')
+    y_fight.append(b)
+    x_fight.append(a)
+x_fight=np.vstack(x_fight)
+y_fight=np.hstack(y_fight)
+y_fight=np.asarray(y_fight,dtype='uint8')
 #%%
-DATA_DIR = "/home/ubuntu/mlproj/target/train/NonFight/"
-x_train_nonfight =[]
-y_train_nonfight=[]
+DATA_DIR = "/home/ubuntu/mlproj/video/train/NonFight/"
+x_nonfight =[]
+y_nonfight=[]
 for path in [f for f in os.listdir(DATA_DIR) if f[-4:] == ".npy"]:
     a = np.load(DATA_DIR + path)
     a=a.reshape(len(a),-1)
     b=np.zeros(len(a))
-    y_train_nonfight.append(b)
-    x_train_nonfight.append(a)
-x_train_nonfight=np.vstack(x_train_nonfight)
-y_train_nonfight=np.hstack(y_train_nonfight)
-y_train_nonfight=np.asarray(y_train_nonfight,dtype='uint8')
+    y_nonfight.append(b)
+    x_nonfight.append(a)
+x_nonfight=np.vstack(x_nonfight)
+y_nonfight=np.hstack(y_nonfight)
+y_nonfight=np.asarray(y_nonfight,dtype='uint8')
 #%%
-DATA_DIR = "/home/ubuntu/mlproj/target/val/Fight/"
+y=np.hstack((y_fight,y_nonfight))
+y = to_categorical(y, num_classes=2)
+print(y.shape)
+#%%
+from sklearn.preprocessing import scale
+x=np.vstack((x_fight,x_nonfight))
+x=scale(x)
+print(x.shape)
+#%%
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
+#%%
+'''
+DATA_DIR = "/home/ubuntu/mlproj/video/val/Fight/"
 x_test_fight =[]
 y_test_fight=[]
 for path in [f for f in os.listdir(DATA_DIR) if f[-4:] == ".npy"]:
@@ -41,7 +54,7 @@ x_test_fight=np.vstack(x_test_fight)
 y_test_fight=np.hstack(y_test_fight)
 y_test_fight=np.asarray(y_test_fight,dtype='uint8')
 #%%
-DATA_DIR = "/home/ubuntu/mlproj/target/val/NonFight/"
+DATA_DIR = "/home/ubuntu/mlproj/video/val/NonFight/"
 x_test_nonfight =[]
 y_test_nonfight=[]
 for path in [f for f in os.listdir(DATA_DIR) if f[-4:] == ".npy"]:
@@ -65,6 +78,7 @@ x_train=np.vstack((x_train_fight,x_train_nonfight))
 x_test=np.vstack((x_test_fight,x_test_nonfight))
 x_train,x_test=x_train/255,x_test/255
 print(x_train.shape,x_test.shape)
+'''
 #%%
 from keras.initializers import glorot_uniform
 from keras.models import Sequential, Model
@@ -73,23 +87,16 @@ from keras.optimizers import Adam,Nadam,SGD,RMSprop
 from keras.callbacks import EarlyStopping
 #%%
 model = Sequential()
-model.add(Dense(200, input_dim=18000, activation='relu'))
-model.add(Dropout(0.1))
-#model.add(BatchNormalization())
-model.add(Dense(300, activation='relu'))
-model.add(Dropout(0.1))
-#model.add(BatchNormalization())
-model.add(Dense(400, activation='relu'))
-model.add(Dropout(0.1))
-#model.add(BatchNormalization())
-model.add(Dense(500, activation='relu'))
-model.add(Dropout(0.1))
+model.add(Dense(100, input_dim=12500, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(200, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(2, activation = 'sigmoid'))
-model.compile(loss='BinaryCrossentropy', optimizer=SGD(lr=0.005), metrics=['Accuracy'])
-from keras.callbacks import ModelCheckpoint
-es = EarlyStopping(monitor='val_loss',mode='min',patience=10,restore_best_weights=True)
-mc = ModelCheckpoint('mlp_cying4.h5', monitor='val_loss', mode='max',save_best_only=True)
-model.fit(x_train, y_train, batch_size=50, epochs=2000, validation_data=(x_test, y_test), callbacks=[es, mc])
+model.compile(loss='BinaryCrossentropy', optimizer=Adam(lr=0.001), metrics=['Accuracy'])
+#from keras.callbacks import ModelCheckpoint
+#es = EarlyStopping(monitor='val_loss',mode='min',patience=10,restore_best_weights=True)
+#mc = ModelCheckpoint('mlp_cying4.h5', monitor='val_loss', mode='max',save_best_only=True)
+model.fit(x_train, y_train, batch_size=512, epochs=200, validation_data=(x_test, y_test))
 #%%
 from sklearn.metrics import cohen_kappa_score, f1_score
 print("Final accuracy on validations set:", 100*model.evaluate(x_test, y_test)[1], "%")
@@ -97,3 +104,13 @@ print("Cohen Kappa", cohen_kappa_score(np.argmax(model.predict(x_test),axis=1),n
 print("F1 score", f1_score(np.argmax(model.predict(x_test),axis=1),np.argmax(y_test,axis=1), average = 'macro'))
 #%%
 model.save('mlp_cying4.hdf5')
+#%%
+predict=np.argmax(model.predict(x_test),axis=1)
+raw=np.argmax(y_test,axis=1)
+#%%
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_pred = predict,y_true = raw)
+print('confusion matrix')
+print(cm)
+
+
